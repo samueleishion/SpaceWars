a878ab26a439f156b7a4289d31c24c38cd5779f3
Sam Acuna <samueleishion@gmail.com>
2014-08-22 23:57:33 -0500

    Added explosions and naive enemy location pointer

---
+++ Assets/Scripts/LocationNotification.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class LocationNotification : MonoBehaviour {
+
+	public void Start() {
+		renderer.material.color = new Color(1,0,0); 
+	}
+
+}
--- Assets/Scripts/ShipBehaviour.cs
+++ Assets/Scripts/ShipBehaviour.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class ShipBehaviour : MonoBehaviour {
+
+	public GameObject explosion; 
+
 	public void OnTriggerEnter(Collider other) {
+		GameObject e = Instantiate(explosion, transform.position, transform.rotation) as GameObject; 
 		Destroy(other.gameObject); 
 		Destroy(gameObject); 
+		Destroy(e,2); 
 	}
 }
