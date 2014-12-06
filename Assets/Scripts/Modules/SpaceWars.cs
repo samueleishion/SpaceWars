b6885f80000b4d6f35c7e4cb9f26a4c4e17475cb
Sam Acuna <samueleishion@gmail.com>
2014-12-06 07:46:04 -0600

    Added intro scene and basic ui buttons

--- Assets/Scripts/Modules/SpaceWars.cs
+++ Assets/Scripts/Modules/SpaceWars.cs
@@ -6,10 +6,29 @@ using Modules;
 
 namespace Modules { 
 	public class SpaceWars : MonoBehaviour { 
-		public static List<GameObject> ships; 
+		// public static List<GameObject> ships; 
+		public GameObject enemy_weak; 
+		public GameObject enemy_strong; 
+		public GameObject player; 
 
 		void Awake() {
-			ships = new List<GameObject>(); 
+			// ships = new List<GameObject>(); 
+			GameObject model; 
+			int i; 
+
+
+			for(i=0; i<25; i++) {
+				model = (GameObject) Object.Instantiate(enemy_weak, new Vector3(0,1.004f+(i*0.02f),0), Quaternion.identity); 
+				((GameObject) model).transform.Rotate(Vector3.up, 180); 
+				((GameObject) model).transform.RotateAround(Vector3.zero, Vector3.left, enemy_weak.transform.position.y); 
+			}
+
+			
+			for(i=0; i<3; i++) {
+				model = (GameObject) Object.Instantiate(enemy_weak, new Vector3(0,1.004f+(i*0.02f),0), Quaternion.identity); 
+				((GameObject) model).transform.Rotate(Vector3.up, 180); 
+				((GameObject) model).transform.RotateAround(Vector3.zero, Vector3.left,  enemy_strong.transform.position.y); 
+			}
 		}
 	}
 }
